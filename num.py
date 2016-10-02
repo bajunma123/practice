0b90a0f143582e222d8d4de8c2d1cbd43364a498
bajunma123 <wangguozhen5612@gmail.com>
2016-10-02 21:35:29 +0800

    Update num.py

--- num.py
+++ num.py
@@ -19,9 +19,13 @@ if len(m) > k:
 
 method 2:
 m = num * 64
-we = [[m[a],m[a+1],m[a+2],m[a+3]] for a in range(len(m)) if a % 4 == 0]
+#we = [[m[a],m[a+1],m[a+2],m[a+3]] for a in range(len(m)) if a % 4 == 0]
+we = [[m[a],m[a+1],m[a+2],m[a+3]] for a, _ in enumrate(m) if a % 4 == 0]
 for i in we:
     for a in i:
         print(a, end=' ')
     print('\n')
+    
+    
+    
      
